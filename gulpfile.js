efb2380bf2bad974c257fce1b342485fb8c9d9cf
Timofte Alexandru <timofte.alexandru05@yahoo.com>
2017-01-06 16:54:01 +0200

    react+express application setup

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    LiveServer = require('gulp-live-server'),
+    browserSync = require('browser-sync'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    reactify = require('reactify');
+
+gulp.task('live-server', function(){
+   var server = new LiveServer('server/main.js');
+   server.start();
+});
+
+gulp.task('bundle',function(){
+    return browserify({
+        entries:'app/main.jsx',
+        debug:true
+    })
+        .transform(reactify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./.tmp'));
+});
+gulp.task('serve',['live-server'],function(){
+   browserSync.init(null,{
+       proxy:"http://localhost:7777",
+       port: 9001
+   })
+});
---
+++ server/main.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = new express();
+
+app.get('/', function(req,res){
+    res.render('./../app/index.ejs',{});
+})
+    .use(express.static(__dirname + './../.tmp'))
+    .listen(7777);
