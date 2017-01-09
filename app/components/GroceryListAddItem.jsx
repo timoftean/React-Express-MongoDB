4d4cc2f04e6e89de81a3c82211a2ea1ac5320fb7
Timofte Alexandru <timofte.alexandru05@yahoo.com>
2017-01-09 09:42:06 +0200

    implemented a form

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -4,11 +4,18 @@ module.exports = React.createClass({
     getInitialState:function(){
       return {input:""};
     },
+    handleInputName: function(e){
+        this.setState({input:e.target.value});
+    },
+    additem:function(e){
+        e.preventDefault();
+        console.log("Adding item!",this.state.input);
+    },
     render: function(){
         return (
             <div className="'grocery-addItem">
-                <form>
-                    <input value={this.state.input}/>
+                <form onSubmit={this.additem}>
+                    <input value={this.state.input} onChange={this.handleInputName}/>
                     <button> Add Item</button>
                 </form>
             </div>
